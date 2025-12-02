7db2b4378fa39fc5bd3c419eeeb9ce161e39d3f5
Subhechha Gautam <124330093+subhechhagautam@users.noreply.github.com>
2025-12-02 12:09:18 +0545

    node modules reinstalled

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
